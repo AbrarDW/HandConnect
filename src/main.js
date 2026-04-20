3436d49c9a38ab63d1a48c7d0188eb76fc347f60
AbrarDW <abrar@abrardw.com>
2026-04-20 10:05:04 +0000

    Revert "Ensure app mounts after MediaPipe CDN scripts load"
    This reverts commit e6f8a3c10f0ab8c01b37425e5cb7eb1b728284e2.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,6 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './assets/styles.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/styles.css'
 
-function initApp() {
-  const app = createApp(App);
-  app.mount('#app');
-}
-
-if (window.hands && window.drawingUtils) {
-  initApp();
-} else {
-  const checkInterval = setInterval(() => {
-    if (window.hands && window.drawingUtils) {
-      clearInterval(checkInterval);
-      initApp();
-    }
-  }, 100);
-}
+const app = createApp(App)
+app.mount('#app')
