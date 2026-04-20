e6de48f45b7fe47a5300e7bf25fd6796a6b02c97
AbrarDW <abrar@abrardw.com>
2026-04-20 10:09:32 +0000

    Add check for MediaPipe globals before mounting app

--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/styles.css'
 
-const app = createApp(App)
-app.mount('#app')
+function mountApp() {
+  const app = createApp(App)
+  app.mount('#app')
+}
+
+if (window.hands && window.drawingUtils) {
+  mountApp()
+} else {
+  const checkInterval = setInterval(() => {
+    if (window.hands && window.drawingUtils) {
+      clearInterval(checkInterval)
+      mountApp()
+    }
+  }, 100)
+}
