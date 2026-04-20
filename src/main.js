e0734857856b9c71bf96be981a3a88d9b6bda33b
AbrarDW <abrar@abrardw.com>
2026-04-20 10:35:54 +0000

    Revert "Add check for MediaPipe globals before mounting app"
    This reverts commit e6de48f45b7fe47a5300e7bf25fd6796a6b02c97.

--- src/main.js
+++ src/main.js
@@ -2,18 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/styles.css'
 
-function mountApp() {
-  const app = createApp(App)
-  app.mount('#app')
-}
-
-if (window.hands && window.drawingUtils) {
-  mountApp()
-} else {
-  const checkInterval = setInterval(() => {
-    if (window.hands && window.drawingUtils) {
-      clearInterval(checkInterval)
-      mountApp()
-    }
-  }, 100)
-}
+const app = createApp(App)
+app.mount('#app')
